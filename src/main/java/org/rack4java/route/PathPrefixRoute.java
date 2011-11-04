711b763fdef669a02afcd57d50e627aa01588503
Frank Carver <frank.carver@googlemail.com>
2011-11-04 12:12:03 +0000

    moved path trimming side-effect out of match and into call

--- src/main/java/org/rack4java/route/PathPrefixRoute.java
+++ src/main/java/org/rack4java/route/PathPrefixRoute.java
@@ -3,6 +3,7 @@ package org.rack4java.route;
 import java.util.Map;
 
 import org.rack4java.Rack;
+import org.rack4java.RackResponse;
 
 public class PathPrefixRoute extends AbstractRoute {
 	public static final String ORIGINAL_PATH_INFO = "ORIGINAL_" + Rack.PATH_INFO;
@@ -19,15 +20,18 @@ public class PathPrefixRoute extends AbstractRoute {
 	@Override public boolean match(Map<String, Object> env) {
 		String path = (String) env.get(Rack.PATH_INFO);
 		if (null == path || null == prefix) return false;
-		boolean ret = path.startsWith(prefix);
-		
-		if (ret && remove) {
+		return path.startsWith(prefix);
+	}
+	
+	@Override public RackResponse call(Map<String, Object> env) throws Exception {
+		String path = (String) env.get(Rack.PATH_INFO);
+		if (remove) {
 			env.put(ORIGINAL_PATH_INFO, path);
 			String tail = path.substring(prefix.length());
 			if (!tail.startsWith("/")) tail = "/" + tail;
 			env.put(PATH_INFO, tail);
 		}
 		
-		return ret;
+		return super.call(env);
 	}
 }
